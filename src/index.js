126b94cdd0457ccb6ec9ad2442bfd091a865083b
Manav <manav.sg1@gmail.com>
2021-09-22 11:04:35 +1000

    dev

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './scss/index.scss';
-import App from './App';
+import App from './router/app/App';
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
---
+++ src/router/app/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+	return (
+		<div className="app-container">
+			<header>
+				<h1>Vibe Check</h1>
+			</header>
+			
+			<section>
+				<button className="btn mt-10"><i className="fa fa-coffee"></i>Hi</button>
+			</section>
+
+			<footer>
+
+			</footer>
+		</div>
+	);
+}
+
+export default App;
