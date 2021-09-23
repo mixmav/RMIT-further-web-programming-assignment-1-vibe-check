aa8e53af953d085abd77bbc8484d3c4234e29e72
Manav Gadhoke <manav.sg1@gmail.com>
2021-09-24 09:26:12 +1000

    dev

--- src/App/Router/Posts/ChoosePictureDialog.js
+++ src/App/Router/Posts/ChoosePictureDialog.js
@@ -1,7 +1,9 @@
 import { useRef, useState, useEffect } from 'react';
-
+import imagePreviewSVG from 'img/svg/image-preview.svg';
 
 function ChoosePictureDialog(props){
+	const [imgURL, setImgURL] = useState("");
+	const [imgError, setImgError] = useState(false);
 	const containerRef = useRef();
 	
 	const handleClick = (event) => {
@@ -14,11 +16,37 @@ function ChoosePictureDialog(props){
 		}
 	}
 
+	const handleAddImage = (event) => {
+		event.preventDefault();
+	}
+
+	useEffect(() => {
+
+	});
+
+	const generateImgSrc = () => {
+		if (imgURL.trim() !== "" && !imgError) {
+			return imgURL;
+		} else {
+			return imagePreviewSVG;
+		}
+	}
+
 	return (
 		<div className="router-page-posts--component-choose-picture-dialog" onClick={checkClickClose} ref={containerRef}>
 			<div className="dialog custom-scrollbar">
-				<h1>Choose a picture</h1>
-				<input type="url" className="text-input full-width mt-10" placeholder="URL of image" />
+				<div className="top-bar">
+					<h1>Choose a picture</h1>
+					<button className="btn darkBlack small" onClick={handleClick}><i className="fa fa-window-close"></i>Close</button>
+				</div>
+
+				<form onSubmit={handleAddImage}>
+					<input onChange={e => setImgURL(e.target.value)} type="url" className="text-input full-width mt-20" placeholder="URL of image" required/>
+					<button type="submit" className="btn mt-20 full-width"><i className="fa fa-plus-square"></i>Add image to post</button>
+				</form>
+				
+				<h1 className="mt-10">Preview</h1>
+				<img onLoad={() => setImgError(false)} onError={() => setImgError(true)} src={generateImgSrc()} alt="User image" />
 			</div>
 		</div>
 	);
